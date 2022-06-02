bb733a120ab102bed30041fb8c4e0fa5c400a578
Sagar Budhathoki <sbmagar.sbm99@gmail.com>
2022-06-02 17:43:31 +0545

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,21 +1,10 @@
 # Create elasticsearch backup s3 bucket
-resource "aws_s3_bucket" "gallerist_es_bucket" {
-  bucket = "gallerist-es-backup-main"
+resource "aws_s3_bucket" "example_es_bucket" {
+  bucket = "example-es-backup"
   acl    = "private"
 
   tags = {
-    Name        = "Gallerist ElasticSearch Backup Bucket"
+    Name        = "Example ElasticSearch Backup Bucket"
     Environment = terraform.workspace
   }
 }
-
-# # Create a snapshot repository
-# resource "elasticsearch_snapshot_repository" "gallerist_repo" {
-#   name = "gallerist-es-backup"
-#   type = "s3"
-#   settings = {
-#     bucket   = aws_s3_bucket.gallerist_es_bucket.bucket
-#     region   = "ap-south-1"
-#     role_arn = "arn:aws:iam::463432632382:role/elasticsearch_iam_role"
-#   }
-# }
